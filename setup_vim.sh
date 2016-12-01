e64d12fd64061c09e21413c044015358547b9947
Philip Poten <philip.poten@gmail.com>
2016-12-01 18:11:23 +0100

    make directories, symlink vimrc by default

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+mkdir -p ~/.vim/bundle/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp ./vimrc ~/.vim/vimrc
-ln ~/.vimrc ~/.vim/vimrc
+ln vimrc ~/.vim/vimrc
 vim +PluginInstall +qall
 
 echo "dont forget to install / update YoucompleteMe"
